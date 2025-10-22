bdf5644c46750d2422726dfc7e442deca13d0872
gitxandert <alexandertoth96@yahoo.com>
2025-10-22 14:34:09 -0400

    modularize MPEG functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::fs::File;
+use std::io::{self, Read, SeekFrom, prelude::*};
 use std::ops::{Shl, BitOr, AddAssign};
 
 // helper to parse groups of num bytes
@@ -108,3 +109,36 @@ pub mod wav {
     }
 
 }
+
+pub mod mpeg {
+    use super::*;
+
+    pub fn parse(path: &str) -> io::Result<()> {
+        let mut f = File::open(path)?;
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf)?;
+        let mut buf_iter = buf.iter().copied().peekable();
+
+        while let Some(b) = buf_iter.next() {
+            if b == 0xFF {
+                if let Some(&next) = buf_iter.peek() {
+                    if next & 0xE0 == 0xE0 {
+                        parse_header(&mut buf_iter);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn parse_header<I>(it: &mut I)
+    where I: Iterator<Item = u8>, {
+        println!("Parsing header:");
+        for _ in 0..3 {
+            println!("{:#X}", it.next().unwrap());
+        }
+        println!("");
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
-use std::fs::File;
-use std::io::{self, Read, SeekFrom, prelude::*};
-use std::fmt::UpperHex;
+use std::io;
 
-use audio_decoder::{parse_bytes, wav};
+use audio_decoder::{wav, mpeg};
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("assets/lazy_beat.mp3")?;
-    let mut buf = Vec::new();
-    f.read_to_end(&mut buf)?;
-    let mut buf_iter = buf.iter().copied().peekable();
-
-    while let Some(b) = buf_iter.next() {
-        if b == 0xFF {
-            if let Some(&next) = buf_iter.peek() {
-                if next & 0xE0 == 0xE0 {
-                    parse_header(&mut buf_iter);
-                }
-            }
-        }
-    }
+    mpeg::parse("assets/lazy_beat.mp3")?;
 
     Ok(())
-}
-
-fn parse_header<I>(it: &mut I)
-where I: Iterator<Item = u8>, {
-    println!("Parsing header:");
-    for _ in 0..3 {
-        println!("{:#X}", it.next().unwrap());
-    }
-    println!("");
 }
