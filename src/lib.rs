73091c9f3c69cfb37648eb08c1dd8f78222c147f
gitxandert <alexandertoth96@yahoo.com>
2025-10-22 11:53:55 -0400

    move wav parsing into src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use std::io;
+use std::ops::{Shl, BitOr, AddAssign};
+
+// helper to parse groups of num bytes
+pub fn parse_bytes<I, T>(bytes: &mut I, num: usize) -> io::Result<T>
+where
+    I: Iterator<Item = io::Result<u8>>,
+    T: From<u8> + Shl<u32, Output = T> + BitOr<Output = T> + AddAssign + Copy + Default,
+{
+    let mut value = T::default();
+
+    for i in 0..num {
+        let b = bytes.next().ok_or_else(|| io::Error::new(io::ErrorKind::UnexpectedEof, "EOF"))??;
+        value += T::from(b) << (i as u32 * 8);
+    }
+
+    Ok(value)
+}
+
+pub mod wav {
+    use super::*;
+
+    // format codes
+    #[repr(u16)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub enum FormatCode {
+        WaveFormatPcm = 0x0001,
+        WaveFormatIeeeFloat = 0x0003,
+        WaveFormatAlaw = 0x0006,
+        WaveFormatMulaw = 0x0007,
+        WaveFormatExtensible = 0xFFFE,
+    }
+
+    impl FormatCode {
+        pub fn from_u16(value: u16) -> Option<Self> {
+            match value {
+                0x0001 => Some(Self::WaveFormatPcm),
+                0x0003 => Some(Self::WaveFormatIeeeFloat),
+                0x0006 => Some(Self::WaveFormatAlaw),
+                0x0007 => Some(Self::WaveFormatMulaw),
+                0xFFFE => Some(Self::WaveFormatExtensible),
+                _ => None,
+            }
+        }
+    }
+
+    pub fn parse<I: Iterator<Item = io::Result<u8>>>(mut bytes: I) -> io::Result<()> {
+        // "fmt "
+        println!("\n");
+        for _ in 1..=4 {
+            if let Some(Ok(b)) = bytes.next() {
+                print!("{}", char::from(b));
+            }
+        }
+        println!("");
+
+        let fmt_size: u32 = parse_bytes(&mut bytes, 4)?;
+        println!("Chunk size: {}", fmt_size);
+
+        let Some(fmt_tag) = FormatCode::from_u16(parse_bytes(&mut bytes, 2)?)
+        else {
+            eprintln!("Error parsing format tag");
+            return Ok(())
+        };
+        println!("Format code: {fmt_tag:?}");
+
+        let n_chan: u32 = parse_bytes(&mut bytes, 2)?;
+        println!("Num channels: {n_chan}");
+
+        let sample_rate: u32 = parse_bytes(&mut bytes, 4)?;
+        println!("Sample rate: {sample_rate}");
+
+        let data_rate: u32 = parse_bytes(&mut bytes, 4)?;
+        println!("Ave bytes /sec: {data_rate}");
+
+        let data_blk_sz: u32 = parse_bytes(&mut bytes, 2)?;
+        println!("Block size: {data_blk_sz}");
+
+        let bits_per: u32 = parse_bytes(&mut bytes, 2)?;
+        println!("Bits per sample: {bits_per}");
+
+        // if !WaveFormatPcm (i.e. is extensible)
+        if fmt_size >= 18 {
+            // extension is either 0 or 22
+            let cb_size: u32 = parse_bytes(&mut bytes, 2)?;
+            println!("Extension size: {cb_size}");
+
+            if cb_size > 0 {
+                let valid_bits: u32 = parse_bytes(&mut bytes, 2)?;
+                println!("Valid bits per sample: {valid_bits}");
+
+                let dw_channel_mask: u32 = parse_bytes(&mut bytes, 4)?;
+                println!("Speaker position mask: {dw_channel_mask}");
+
+                let old_fmt: u32 = parse_bytes(&mut bytes, 2)?;
+                println!("GUID: {old_fmt}");
+
+                // skip over Microsoft stuff
+                // TODO: compare against audio media subtype
+                for _ in 0..14 {
+                    if let Some(Ok(b)) = bytes.next() {}
+                }
+                print!("\n");
+            }
+        }
+
+        Ok(())
+    }
+
+}
