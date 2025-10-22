edc48ca664117ed3433367004b9a3ed6589a9e9a
gitxandert <alexandertoth96@yahoo.com>
2025-10-22 15:31:57 -0400

    implement extension matching

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,31 @@ use std::io;
 
 use audio_decoder::{wav, mpeg};
 
-fn main() -> io::Result<()> {
-    mpeg::parse("assets/lazy_beat.mp3")?;
+fn main() -> Result<(), ParseError<'static>> {
+    let path = "assets/lazy_beat.mp3";
+    let ext: &str = match path.rsplit_once(|b: char| b == '.') {
+        Some((before, after)) if !before.is_empty() && !after.is_empty() => after,
+        _ => "",
+    };
+
+    match ext {
+        "mp3" => mpeg::parse(path),
+        "wav" => wav::parse(path),
+        _ => return Err(ParseError::UnsupportedFormat(ext)),
+    }?;
 
     Ok(())
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum ParseError<'a> {
+    UnsupportedFormat(&'a str),
+    Io(io::Error),
+}
+
+impl From<io::Error> for ParseError<'_> {
+    fn from(e: io::Error) -> Self {
+        Self::Io(e)
+    }
 }
