46578afc4331fb98bb96ee37068c30cd789c34b7
gitxandert <alexandertoth96@yahoo.com>
2025-11-04 16:03:37 -0500

    create WAV parsing test

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,20 @@ pub mod mpeg;
 pub mod aiff;
 pub mod wav;
 pub mod decode_helpers;
+
+use decode_helpers::{DecodeResult, DecodeError, AudioFile};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_wav() {
+        let path = "assets/fairies.wav";
+
+        match wav::parse(path) {
+            Ok(file) => println!("{:?}", file),
+            Err(error) => eprintln!("{:?}", error),
+        };
+    }
+}
