90658d0c0451b2a536e20436a44147f9c092c8f4
gitxandert <alexandertoth96@yahoo.com>
2025-11-14 18:59:04 -0500

    take over terminal for fun REPL marker

--- src/playback.rs
+++ src/playback.rs
@@ -1,8 +1,10 @@
+use std::os::unix::io::AsRawFd;
+use libc::{termios, tcgetattr, tcsetattr, cfmakeraw, TCSANOW};
 use std::{
-    io::{self, Write},
+    io::{self, Read, Write},
     sync::{Arc, Mutex},
     thread,
-    time::Duration,
+    time::{Duration, Instant},
 };
 use alsa::{
     Direction, ValueOr,
@@ -46,33 +48,78 @@ pub fn play_file(af: AudioFile) {
     pcm.sw_params(&swp).unwrap();
     */
 
+    let marker = Arc::new(Mutex::new(0usize));
+    let buffer = Arc::new(Mutex::new(String::new()));
+    let repl_chars = ['^', 'X', 'v', '>', 'X', '<', 'Z'];
+
+    {
+        let marker = marker.clone();
+        let buffer = buffer.clone();
+        thread::spawn(move || { 
+            loop {
+                {
+                    let mut m = marker.lock().unwrap();
+                    *m = (*m + 1) % repl_chars.len();
+                }
+
+                {
+                    let m = *marker.lock().unwrap();
+                    let input_text = buffer.lock().unwrap().clone();
+                    print!("\r{} {}", repl_chars[m], input_text);
+                    std::io::stdout().flush().unwrap();
+                }
+
+                thread::sleep(Duration::from_millis(100));
+            }
+        });
+    }
+    
     let state = Arc::new(Mutex::new(PlayState::Stopped));
     let state_for_repl = Arc::clone(&state);
 
     println!("");
-    thread::spawn(move || {
-        loop {
-            print!("> ");
-            io::stdout().flush().unwrap();
-            let mut cmd = String::new();
-            io::stdin()
-                .read_line(&mut cmd)
-                .expect("Failed to read command");
-            let cmd = cmd.trim();
-            let mut s = state_for_repl.lock().unwrap();
-            match cmd {
-                "start" => *s = PlayState::Running,
-                "pause" => *s = PlayState::Paused,
-                "stop" => *s = PlayState::Stopped,
-                "q" | "quit" => {
-                    *s = PlayState::Quit;
-                    break;
-                }
-                _ => println!("Unknown command '{cmd}'"),
-            }
-        }
-    });
 
+    raw_mode("on");
+
+    {
+        let buffer = buffer.clone();
+        thread::spawn(move || {
+            loop {
+                let c = read_char();
+
+                match c {
+                    b'\n' | b'\r' => {
+                        // enter
+                        print!("\n");
+                        let mut buf = buffer.lock().unwrap();
+
+                        let mut s = state_for_repl.lock().unwrap();
+                        match buf.as_str() {
+                            "start" => *s = PlayState::Running,
+                            "pause" => *s = PlayState::Paused,
+                            "stop" => *s = PlayState::Stopped,
+                            "q" | "quit" => {
+                                *s = PlayState::Quit;
+                                break;
+                            }
+                            _ => println!("Unknown command '{}'", *buf),
+                        }
+                        buf.clear();
+                    }
+                    127 => {
+                        // backspace
+                        let mut buf = buffer.lock().unwrap();
+                        buf.pop();
+                    }
+                    _ => {
+                        let mut buf = buffer.lock().unwrap();
+                        buf.push(c as char);
+                    }
+                }
+            }
+        });
+    }
+    
     let mut idx = 0;
     loop {
         let s = *state.lock().unwrap();
@@ -116,5 +163,39 @@ pub fn play_file(af: AudioFile) {
     }
 
     pcm.drop().unwrap();
-    pcm.drain().unwrap();
+    pcm.drain().unwrap();    
+    raw_mode("off");
+    print!("\n");
+
+}
+
+static mut ORIG_TERM: Option<termios> = None;
+
+fn raw_mode(switch: &str) {
+    unsafe {
+        let fd = libc::STDIN_FILENO;
+
+        let mut term: termios = std::mem::zeroed();
+        tcgetattr(fd, &mut term);
+
+        match switch {
+            "on" => {
+                ORIG_TERM = Some(term);
+                let mut raw = term;
+                cfmakeraw(&mut raw);
+                tcsetattr(fd, TCSANOW, &raw);
+            }
+            _ => {
+                if let Some(orig) = ORIG_TERM {
+                    tcsetattr(fd, TCSANOW, &orig);
+                }
+            }
+        };
+    }
+}
+
+fn read_char() -> u8 {
+    let mut buf = [0u8; 1];
+    std::io::stdin().read_exact(&mut buf).unwrap();
+    buf[0]
 }
