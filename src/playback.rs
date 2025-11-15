88c145a64555c5d0c0571dcc8b63c7f24f411f37
gitxandert <alexandertoth96@yahoo.com>
2025-11-14 20:47:41 -0500

    improve engine/REPL relationship

--- src/playback.rs
+++ src/playback.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{self, Write},
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}},
     thread,
     time::Duration,
 };
@@ -13,6 +13,7 @@ use crate::decode_helpers::AudioFile;
 pub fn play_file(af: AudioFile) {
     /*
      * struct AudioFile {
+     *     file_name: String,
      *     format: String,      // file type (WAV, AIFF)
      *     sample_rate: u32,
      *     num_channels: u32,
@@ -34,12 +35,15 @@ pub fn play_file(af: AudioFile) {
     
     let period_size = hwp.get_period_size().unwrap() as usize;
     let num_channels = hwp.get_channels().unwrap() as usize;
- 
-    let conductor = Arc::new(Mutex::new(Conductor::prepare(num_channels, period_size)));
+
+    let mut tracks: Vec<AudioFile> = Vec::new();
+    println!("Adding {} to tracks", af.file_name);
+    tracks.push(af);
+    let conductor = Arc::new(Mutex::new(Conductor::prepare(num_channels, period_size, tracks)));
     let cond_for_repl = Arc::clone(&conductor);
 
-    let running = Arc::new(Mutex::new(true));
-    let running_for_repl = Arc::clone(&running);
+    let running = Arc::new(AtomicBool::new(true));
+    let running_for_repl = running.clone();
 
     println!("");
     thread::spawn(move || {
@@ -51,14 +55,18 @@ pub fn play_file(af: AudioFile) {
                 .read_line(&mut cmd)
                 .expect("Failed to read command");
             let cmd = cmd.trim();
-            let con = cond_for_repl.lock().unwrap();
-            let mut r = running_for_repl.lock().unwrap();
+            let mut parts = cmd.splitn(2, ' ');
+            let cmd = parts.next().unwrap();
+            let args = parts.next().unwrap_or_else(|| "");
+
+            let mut con = cond_for_repl.lock().unwrap();
             match cmd {
-                "start" => con.add_voice(&af),
-                "pause" => con.voices.get(0).active = false,
-                "stop" => con.voices.pop(),
+                "start" => con.add_voice(args),
+                "pause" => con.pause_voice(args),
+                "resume" => con.resume_voice(args),
+                "stop" => con.stop_voice(args),
                 "q" | "quit" => {
-                    *r = false;
+                    running_for_repl.store(false, Ordering::SeqCst);
                     break;
                 }
                 _ => println!("Unknown command '{cmd}'"),
@@ -66,11 +74,18 @@ pub fn play_file(af: AudioFile) {
         }
     });
 
+    if pcm.state() != State::Running {
+        pcm.start().unwrap();
+    }
     let mut out_buf = vec![0i16; period_size * num_channels];
-
-    while running {
-        let con = conductor.lock().unwrap();
+    while running.load(Ordering::SeqCst) {
+        let mut con = conductor.lock().unwrap();
         con.coordinate(&mut out_buf);
+        drop(con);
+    
+        // for some reason, this only plays when I stop the
+        // track that is being written to the buffer; the track
+        // also plays back twice as fast
         io.writei(&out_buf).unwrap();
     }
 
@@ -82,13 +97,17 @@ struct Conductor {
     voices: Vec<Voice>,
     out_channels: usize,
     period_size: usize,
+    tracks: Vec<AudioFile>
 }
 
 impl Conductor {
-    fn prepare(out_channels: usize, period_size: usize) -> Self {
-        let voices: Vec<Voice> = Vec::new();
-
-        Self { voices, out_channels, period_size }
+    fn prepare(out_channels: usize, period_size: usize, tracks: Vec<AudioFile>) -> Self {
+        Self { 
+            voices: Vec::<Voice>::new(), 
+            out_channels, 
+            period_size,
+            tracks
+        }
     }
 
     fn coordinate(&mut self, out_buf: &mut [i16]) {
@@ -109,14 +128,57 @@ impl Conductor {
             }
         }
     }
+
+    fn add_voice(&mut self, name: &str) {
+        for track in &self.tracks {
+            if track.file_name.as_str() == name {
+                self.voices.push(Voice::new(&track));
+                return;
+            }
+        }
+        eprintln!("Err: Could not find track '{name}'");
+    }
+
+    fn pause_voice(&self, name: &str) {
+        for voice in &self.voices {
+            if voice.name == name {
+                voice.active == false;
+                return;
+            }
+        }
+        eprintln!("Err: Could not find voice '{name}'");
+    }
+
+    fn resume_voice(&self, name: &str) {
+        for voice in &self.voices {
+            if voice.name == name {
+                voice.active == true;
+                return;
+            }
+        }
+        eprintln!("Err: Could not find voice '{name}'");
+    }
+
+    fn stop_voice(&mut self, name: &str) {
+        let mut i = 0;
+        while i < self.voices.len() {
+            if self.voices[i].name == name {
+                self.voices.remove(i);
+                return;
+            }
+            i += 1;
+        }
+        eprintln!("Err: Could not find voice '{name}'");
+    }
 }
 
 struct Voice {
+    name: String,
     samples: Arc<Vec<i16>>,
     sample_rate: u32,
     channels: usize,
-    position: f32, 
-    speed: f32,   
+    position: f32,
+    speed: f32,  
     direction: f32,
     gain: f32,
     active: bool,
@@ -125,24 +187,25 @@ struct Voice {
 impl Voice {
     fn new(af: &AudioFile) -> Self {
         Self {
+            name: af.file_name.clone(),
             samples: Arc::new(af.samples.clone()),
             sample_rate: af.sample_rate, 
-            channels: af.num_channels, 
+            channels: af.num_channels as usize, 
             position: 0.0,
             speed: 1.0,
             direction: 1.0, 
             gain: 1.0, 
-            active: false
+            active: true,
         }
     }
 
-    fn process(&mut self, acc: &mut f32; frame: usize, ch: usize) {
-        if !self.active { continue; }
+    fn process(&mut self, acc: &mut f32, frame: usize, mut ch: usize) {
+        if !self.active { return; }
 
         let idx = self.position as usize;
         if idx + 1 >= self.samples.len() / self.channels {
             self.active = false;
-            continue;
+            return;
         }
 
         // if there are more output channels than the track has
@@ -154,10 +217,10 @@ impl Voice {
             if ch < 2 {
                 ch = 0;
             } else {
-                continue;
+                return;
             }
         } else if ch >= self.channels {
-            continue;
+            return;
         }
 
         // linear interpolation
@@ -166,7 +229,7 @@ impl Voice {
         let s1 = self.samples[((idx + 1) * self.channels) + (ch % self.channels)] as f32;
         let sample = s0 * (1.0 - frac) + s1 * frac;
 
-        *acc += sample * voice.gain;
+        *acc += sample * self.gain;
 
         // advance
         self.position += self.direction * self.speed;
